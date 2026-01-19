db393ad7fc77c8b012d84d9e06d2f6c20b857610
Harshad jadhav <miharshad88@gmail.com>
2026-01-19 16:49:55 +0530

    Return dummy token to test login

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -13,7 +13,7 @@ export class AuthService {
     const payload = { username: 'admin', role: 'ADMIN' };
 
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: 'dummy-token',
       user: payload,
       expiresIn: '1h',
     };
